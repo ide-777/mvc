85912274347196b39a00ac9360c963407cdfff29
dergachev <idergachev@kde.link>
2018-06-28 10:07:45 +0300

    fix

--- application/dictionary/ru.php
+++ application/dictionary/ru.php
@@ -2,17 +2,17 @@
 
 define('DEFAULT_TITLE', 'Title');
 define('DEFAULT_DESCRIPTION', 'Description');
-define('DEFAULT_KEYWORDS', 'keywords');
+define('DEFAULT_KEYWORDS', 'Keywords');
 
 define('PAGE_MAIN_TITLE', 'Title');
 define('PAGE_MAIN_DESCRIPTION', 'Description');
-define('PAGE_MAIN_KEYWORDS', 'keywords');
+define('PAGE_MAIN_KEYWORDS', 'Keywords');
 
 define('PAGE_ABOUT_TITLE', 'Title');
 define('PAGE_ABOUT_DESCRIPTION', 'Description');
-define('PAGE_ABOUT_KEYWORDS', 'keywords');
-define('PAGE_ABOUT_TEXT1', 'По всем предложениям или вопросы пишите <a href="mailto:ide-777@yandex.ru">ide-777@yandex.ru</a>.');
+define('PAGE_ABOUT_KEYWORDS', 'Keywords');
+define('PAGE_ABOUT_TEXT1', 'On all offers and questions write <a href="mailto:ide-777@yandex.ru">ide-777@yandex.ru</a>.');
 
 define('PAGE_ERROR404_TITLE', 'Title');
 define('PAGE_ERROR404_DESCRIPTION', 'Description');
-define('PAGE_ERROR404_KEYWORDS', 'keywords');
+define('PAGE_ERROR404_KEYWORDS', 'Keywords');
